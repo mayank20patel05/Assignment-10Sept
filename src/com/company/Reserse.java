f824ff9dcfd3d7b2ca7b74697fc05d9774b75d6d
Mayank Patel <mayank20patel05@gmail.com>
2022-09-16 09:17:54 -0400

    Fifteenth assignment added

--- src/com/company/Reserse.java
+++ src/com/company/Reserse.java
@@ -4,15 +4,22 @@ import java.util.Arrays;
 
 public class Reserse{
     public static void main(String[] args) {
-        reverseString("vaibhavi");
+        String s = "vaibhavi";
+        System.out.println(Arrays.toString(toCharArr(s)));
+        reverseString(s);
     }
     public static void reverseString(String s){
-        char[] chars = new char[s.length()];
-
         for (int i = s.length()-1, j=0; i >=0; i--, j++ ) {
-            chars[j] = s.charAt(i);
             System.out.print(s.charAt(i));
         }
         System.out.println();
+    }
+
+    public static char[] toCharArr(String s){
+        char[] arr = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            arr[i] = s.charAt(i);
+        }
+        return arr;
     }
 }
